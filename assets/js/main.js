5e2c8af75b93bf4504480cf8953a12ebb7356630
Towsif Ahmed Omi <towsif031@gmail.com>
2021-12-22 20:10:12 +0600

    handleStart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log('js loaded');
+document.addEventListener('keypress', handleStart, { once: true });
+const title = document.querySelector('[data-title]');
+
+function handleStart() {
+	title.classList.add('hide');
+}
+
+function handleLose() {}

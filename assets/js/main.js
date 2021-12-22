10e54608f4c675dbbf2c3b4b27f9e84f9ff17ee2
Towsif Ahmed Omi <towsif031@gmail.com>
2021-12-22 20:17:01 +0600

    requestAnimationFrame

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 document.addEventListener('keypress', handleStart, { once: true });
 const title = document.querySelector('[data-title]');
 
+function updateLoop(time) {
+	console.log(time);
+	window.requestAnimationFrame(updateLoop);
+}
+
 function handleStart() {
 	title.classList.add('hide');
+	window.requestAnimationFrame(updateLoop);
 }
 
 function handleLose() {}

260d023b3d315edd758a6d91e2fb361864342da2
Towsif Ahmed Omi <towsif031@gmail.com>
2021-12-23 23:51:00 +0600

    getTop

--- assets/js/bird.js
+++ assets/js/bird.js
@@ -1,3 +1,13 @@
 const birdElem = document.querySelector('[data-bird]');
 
-export function updateBird(delta) {}
+export function updateBird(delta) {
+	console.log(getTop());
+}
+
+function setTop(top) {
+	birdElement.style.setProperty('--bird-top', top);
+}
+
+function getTop() {
+	return getComputedStyle(birdElem).getPropertyValue('--bird-top');
+}

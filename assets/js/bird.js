aaea59fb21105e102b447ca8ef289fec7f417238
Towsif Ahmed Omi <towsif031@gmail.com>
2021-12-23 23:46:10 +0600

    bird js

---
+++ assets/js/bird.js
@@ -0,0 +1,3 @@
+const birdElem = document.querySelector('[data-bird]');
+
+export function updateBird(delta) {}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
+import { updateBird } from './bird.js';
+
 document.addEventListener('keypress', handleStart, { once: true });
 const title = document.querySelector('[data-title]');
 
+let lastTime;
 function updateLoop(time) {
-	console.log(time);
+	if (lastTime == null) {
+		lastTime = time;
+		requestAnimationFrame(updateLoop);
+		return;
+	}
+	const delta = time - lastTime;
+	updateBird(delta);
+	lastTime = time;
 	window.requestAnimationFrame(updateLoop);
 }
 
